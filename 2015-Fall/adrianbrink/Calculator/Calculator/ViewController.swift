346b5660977e1bc35b6b93551534eb133176bb29
adrianbrink <adrianbrink@users.noreply.github.com>
2015-09-06 20:43:50 +0200

    Create a somewhat working calculator. Add computational capabilities. No = functionality, Can't add more than two numbers.

--- 2015-Fall/adrianbrink/Calculator/Calculator/ViewController.swift
+++ 2015-Fall/adrianbrink/Calculator/Calculator/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsTyping: Bool = false
+    var lastAction: Bool?
+    var lastValue = 0
+    var newValue = 0
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -25,27 +28,46 @@ class ViewController: UIViewController {
         }
     }
 
-    @IBAction func clearDisplay(sender: UIButton) {
-        let key = sender.currentTitle!
-        if key == "Del" {
+    @IBAction func clearDisplay() {
+        display.text = "0"
+        lastValue = 0
+        lastAction = false
+        userIsTyping = false
+    }
+    
+    @IBAction func operators(sender: UIButton) {
+        let currentSymbol = sender.currentTitle!
+        var lastSymbol = ""
+        
+        if (lastAction == true) {
+            calculation(lastValue, symbol: lastSymbol)
+        } else {
+            lastValue = (display.text! as NSString).integerValue
+            lastSymbol = currentSymbol
+            lastAction = true
             display.text = "0"
             userIsTyping = false
         }
     }
+    
+    func calculation(lastValue: Int, symbol: String) {
+        var currentValue = (display.text! as NSString).integerValue
 
-    /* Stuff doesn't work yet.
-    @IBAction func addNumbers(sender: UIButton) {
-        userIsTyping = false
-        var firstNumber = ""
-        var firstNumberInt = 0
-        firstNumber = display.text!
-        firstNumberInt = firstNumber
         
-        let key = sender.currentTitle!
-        if key == "+" {
-            
+        switch symbol {
+            case "+":
+                newValue = lastValue + currentValue
+            case "-":
+                newValue = lastValue - currentValue
+            case "*":
+                newValue = lastValue * currentValue
+            case "/":
+                newValue = lastValue / currentValue
+            default:
+                break
         }
+        
+        display.text = "\(newValue)"
     }
-    */
 }
 
