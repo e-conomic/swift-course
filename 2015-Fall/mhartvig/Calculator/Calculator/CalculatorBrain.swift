11521c8431022db68bb6fdeefebb0e3a5dd1d29f
= <martinhartvig@gmail.com>
2015-09-15 16:40:28 +0200

    Initial work on Ass #2

--- 2015-Fall/mhartvig/Calculator/Calculator/CalculatorBrain.swift
+++ 2015-Fall/mhartvig/Calculator/Calculator/CalculatorBrain.swift
@@ -14,6 +14,7 @@ class CalculatorBrian {
     private enum Ops: Printable {
         case value(Double)
         case MathConst(String, Double)
+        case Variable(String)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
         
@@ -28,13 +29,27 @@ class CalculatorBrian {
                     return "\(symbol)"
                 case .MathConst(let symbol, _):
                     return "\(symbol)"
+                case .Variable(let variable):
+                    return variable
                 }
             }
         }
     }
+    
+    var description: String {
+        get {
+            if let desc = evalDescription() {
+                return desc
+            }
+            return ""
+        }
+    }
+    
     private var opStack = [Ops]()
     
     private var knownOps = [String:Ops]()
+    
+    private var variables = [String:Double]()
     
     init() {
         func learn(op: Ops) {
@@ -64,14 +79,65 @@ class CalculatorBrian {
         return "\(opStack)"
     }
     
+    func clear() {
+        opStack = [Ops]()
+        variables = [String:Double]()
+    }
+    
+    func pushOperand(symbol: String) -> Double? {
+        opStack.append(Ops.Variable(symbol))
+        return evaluate()
+    }
+    
+    func setVariable(symbol: String, value: Double) {
+        variables[symbol] = value
+    }
+    
+    func getVariable(symbol: String) -> Double? {
+        return variables[symbol]
+    }
+    
+    private func evalDescription() -> String? {
+        let (result, reminder) = description(opStack)
+        println(result)
+        return result
+    }
+    
+    private func description(ops: [Ops]) -> (Result: String?, ReminderOps: [Ops]) {
+        if (!ops.isEmpty) {
+            var reminderOps = ops
+            let op = reminderOps.removeLast()
+            
+            switch op {
+            case .value(let value):
+                return ("\(value)", reminderOps)
+            case .MathConst(let symbol, _):
+                return ("\(symbol)", reminderOps)
+            case .Variable(let variable):
+                return ("\(variable)", reminderOps)
+            case .UnaryOperation(let symbol, let operation):
+                let evaluatedOp = description(reminderOps)
+                if let res = evaluatedOp.Result {
+                    return ("\(symbol)(\(res))", evaluatedOp.ReminderOps)
+                }
+            case .BinaryOperation(let symbol, let operation):
+                let leftEvaluation = description(reminderOps)
+                if let leftResult = leftEvaluation.Result {
+                    let rightEvaluation = description(leftEvaluation.ReminderOps)
+                    if let rightResult = rightEvaluation.Result {
+                        return ("\(rightResult)\(symbol)\(leftResult)", rightEvaluation.ReminderOps)
+                    }
+                }
+            }
+        }
+        return ("?", ops)
+    }
+    
+    
     func evaluate() -> Double? {
         let (result, reminder) = evaluate(opStack)
         println("\(opStack) = \(result) with \(reminder)")
         return result
-    }
-    
-    func clear() {
-        opStack = [Ops]()
     }
     
     private func evaluate(ops: [Ops]) -> (Result: Double?, ReminderOps: [Ops]) {
@@ -98,6 +164,11 @@ class CalculatorBrian {
                         return (operation(leftResult,rightResult), rightEvaluation.ReminderOps)
                     }
                 }
+            case .Variable(let variable):
+                if let value = variables[variable] {
+                    return (value, reminderOps)
+                }
+                return (nil, ops)
             }
         }
         return (nil, ops)
