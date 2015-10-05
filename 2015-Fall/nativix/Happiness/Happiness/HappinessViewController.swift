e3d7f5bc1418f8ae40b121e1c4b0fd17e5e1bffc
MARIA NATIVIDAD LARA DIAZ <nati@MARIAs-MacBook-Pro.local>
2015-10-06 00:12:35 +0200

    Happines and Calculator2

---
+++ 2015-Fall/nativix/Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by MARIA NATIVIDAD LARA DIAZ on 28/09/15.
+//  Copyright © 2015 Appstract Development. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    var happiness: Int = 50 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    private func updateUI(){
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+}
