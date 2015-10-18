67b21999907ea4828d4e0687156304847179595c
Adrian Brink <adrianbrink@users.noreply.github.com>
2015-10-18 20:12:42 +0200

    Completed dropit, however I can't get it to display on screen for some reason :-(

---
+++ 2015-Fall/adrianbrink/DropIt/DropIt/DropItBehavior.swift
@@ -0,0 +1,53 @@
+//
+//  DropItBehavior.swift
+//  DropIt
+//
+//  Created by aevitas on 18/10/15.
+//  Copyright © 2015 adrian. All rights reserved.
+//
+
+import UIKit
+
+class DropItBehavior: UIDynamicBehavior {
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedCollider = UICollisionBehavior()
+        lazilyCreatedCollider.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedCollider
+        }()
+    
+    lazy var dropBehavior: UIDynamicItemBehavior = {
+       let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = true
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func addDrop(drop: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+        dropBehavior.addItem(drop)
+    }
+    
+    func removeDrop(drop: UIView) {
+        gravity.removeItem(drop)
+        collider.removeItem(drop)
+        dropBehavior.addItem(drop)
+        drop.removeFromSuperview()
+    }
+
+}
